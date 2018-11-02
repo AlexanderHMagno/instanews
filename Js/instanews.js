7c7934803a628d5887ff33b12e7f2b488b990769
Alexanderuser.name <alexander.hortua10@gmail.com>
2018-11-02 16:06:11 -0700

    adding properly menu

--- Js/instanews.js
+++ Js/instanews.js
@@ -1,5 +1,47 @@
 
+var data =  [
+  "home",
+  "opinion",
+  "world",
+  "national",
+  "politics",
+  "upshot",
+  "nyregion",
+  "business",
+  "technology",
+  "science",
+  "health",
+  "sports",
+  "arts",
+  "books",
+  "movies",
+  "theater",
+  "sundayreview",
+  "fashion",
+  "tmagazine",
+  "food",
+  "travel",
+  "magazine",
+  "realestate",
+  "automobiles",
+  "obituaries",
+  "insider"
+];
+$.each(data, function (key, value) {
+
+
+$(".dropdown-content").append("<a href=\"#\">"+value+"</a>");
+
+}); // it shows the initial menu
+
+
+
+
+
+
+
 $(".dropdown-content").on("click", function(){
+
 
   $(".initial").fadeOut(3000);
   $('header').fadeIn(3000);
@@ -11,7 +53,8 @@ $(".dropdown-content").on("click", function(){
 
 
 //This is the button that has to be activated in order to update news.
-$("button").on("click", function () {
+$(".dropdown-content").on("click", function () {
+  
   var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
   url += '?' + $.param({
     'api-key': "90a077fb52414546bcc9e4944c36b24d"
@@ -20,10 +63,10 @@ $("button").on("click", function () {
     url: url,
     method: 'GET',
   }).done(function (data) {
-
+   
     var datas = data.results //this is my main source of info
     var count = 0 // its a counter created in order to build new class.
-
+   
     $.each(datas, function (key, value) {
     //for each news it will create a new container. 
 
@@ -34,8 +77,9 @@ $("button").on("click", function () {
         $(".fullBody").append("<a href=\"" + value.short_url + "\" target=\"_blank\"><div class=\"newsBody " + newClass + "\"><li><div class=\"phantom\"><p>" + value.abstract + "</p></li></div></div></a>");
         newClass = ".newsBody" + count; //adding var as a selector is not neccesary to add more quotations
         $(newClass).css({ "background-image": "url(\"" + imagenes + "\")", "background-size": "cover", "background-repeat": "no-repeat", "background-position": "center" });
-
-
+        $(newClass).fadeIn("slow");
+          
+        
       }
     });
   });
